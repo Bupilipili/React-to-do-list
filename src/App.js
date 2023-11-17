33ed5fb2cffd145708054286b2cbfa014dfca20f
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-11-17 11:43:12 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import TodoList from './components/TodoList'; // Adjust based on your file location
+
+const App = () => {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
